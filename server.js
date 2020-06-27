f9ffc786145f9c63f900d4a67e90b23194250d20
MollyCoddle Tech <spoonfeedertech@gmail.com>
2020-06-27 13:41:22 +0100

    heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const serveStatic = require('serve-static');
-const path = require('path');
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
 
 const app = express()
 
@@ -9,4 +9,4 @@ app.use('/', serveStatic(path.join(__dirname, '/dist')))
 const port = process.env.PORT || 8080
 app.listen(port)
 
-console.log(port);
+console.log(port)
